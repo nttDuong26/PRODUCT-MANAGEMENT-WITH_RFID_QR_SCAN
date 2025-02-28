661768f8ffc4c4311718af1b5a70d71bc65b9c1f
Nguyen Tran Thuy Duong <136062387+nttDuong26@users.noreply.github.com>
2025-02-28 11:39:37 +0700

    Update app_config.dart

--- lib/Utils/app_config.dart
+++ lib/Utils/app_config.dart
@@ -1,6 +1,5 @@
 class AppConfig{
-   static String IP = 'http://192.168.19.180:5057';
- // static String IP = 'https://jvf-admin.rynansaas.com';
+   static String IP = 'http://';
   static String? device = 'C5';
   static String? connectedDeviceMac = '';
   static String? connectedDeviceName = '';
